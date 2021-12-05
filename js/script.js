9df1f7d7d730cad73bb649fdd397bef9de392e63
Dariusz Gawron <screbsy+g1@gmail.com>
2021-12-05 18:31:15 +0100

    Add new sounds and keys + improvements

--- js/script.js
+++ js/script.js
@@ -1,6 +1,19 @@
-window.addEventListener('keydown',event => {
-    const audio = document.querySelector(`audio[data-key='${event.keyCode}']`);
-    if(!audio) return
+const playAudio = function(event) {
+    const keyLetter = String.fromCharCode(event.keyCode);
+    const key = document.querySelector(`.keyboard__key[data-key='${keyLetter}']`);
+    const audio = document.querySelector(`audio[data-key='${keyLetter}']`);
+    if(!audio || !key) return;
+    key.classList.add('keyboard__key--active');
     audio.currentTime=0;
     audio.play();
-});
+    
+};
+const removeActive = function(event) {
+    if(event.propertyName!=='transform') return;
+    this.classList.remove('keyboard__key--active');
+};
+
+const keys = document.querySelectorAll('.keyboard__key');
+keys.forEach(key => key.addEventListener('transitionend',removeActive));
+
+window.addEventListener('keydown',playAudio);
